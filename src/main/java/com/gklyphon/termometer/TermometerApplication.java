8f61e0a64c7a8431c18334ec95b00bddeffeae4a
JFCiscoHuerta <151865620+JFCiscoHuerta@users.noreply.github.com>
2024-11-09 14:34:52 -0600

    project setup

---
+++ src/main/java/com/gklyphon/termometer/TermometerApplication.java
@@ -0,0 +1,13 @@
+package com.gklyphon.termometer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TermometerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TermometerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gklyphon/termometer/TermometerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gklyphon.termometer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TermometerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
